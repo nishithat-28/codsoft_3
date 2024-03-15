031891d8fe0b6892d7ffe114cbded5f039ed8b72
nishithat-28 <152367167+nishithat-28@users.noreply.github.com>
2024-03-15 15:54:05 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,12 +1,16 @@
+from re import *
 from random import *
 from string import *
 
 n=int(input("Enter the length of your password: "))
 print()
-s=ascii_letters+digits+punctuation
-l=[i for i in s]
-shuffle(l)
-p=sample(l,n)
-password="".join(p)
 
-print("Your Password: {}\n".format(password))
+l=[i for i in ascii_letters+digits+punctuation]
+
+while True:
+    shuffle(l)
+    p=sample(l,n)
+    password="".join(p)
+    if len(password) >= n and any(c.islower() for c in password) and any(c.isupper() for c in password) and any(c.isdigit() for c in password) and any(c in punctuation for c in password) :
+        print("Your Password: {}\n".format(password))
+        break
